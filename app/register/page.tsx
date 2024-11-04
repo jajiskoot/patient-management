560322d776805089cfea982d6b241e52638fee8c
Jacob Jiskoot <jajiskoot@yahoo.com>
2024-11-04 13:28:18 -0500

    Professional Sign in and Registration headers

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -36,8 +36,7 @@ export default function Register() {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Pray tell, who be this gallant soul seeking entry to mine humble
-            abode?
+            Account Registration
           </h1>
           <form
             onSubmit={handleSubmit}
@@ -49,7 +48,7 @@ export default function Register() {
                 htmlFor="email"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your email
+                Email
               </label>
               <input
                 type="email"
